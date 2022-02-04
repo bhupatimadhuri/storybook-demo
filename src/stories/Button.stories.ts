8187062156c81ab635be33e5aef63de427dab6b1
Madhuri Bhupathi <Madhuri.Bhupathi@vu.com>
2022-02-04 16:00:12 -0600

    Second commit

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -21,7 +21,7 @@ export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/angular/writing-stories/args
 Primary.args = {
   primary: true,
-  label: 'Button',
+  label: 'Primary Button',
 };
 
 export const Secondary = Template.bind({});
